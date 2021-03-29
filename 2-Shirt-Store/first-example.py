04524e4fcc341320ae53ca93291ad3ca655178f1
diwakar <diwakarsingh052@gmail.com>
2021-03-30 00:48:50 +0530

    .

--- 2-Shirt-Store/first-example.py
+++ 2-Shirt-Store/first-example.py
@@ -1,6 +1,11 @@
 import redis
 
 r = redis.Redis('192.168.99.100')
-r.set("name", "Diwakar")
-name = r.get("name")
-print(name)
+r.set("fname","Diwakar")
+name_bytes = r.get("fname")
+
+name = name_bytes.decode('utf-8')
+msg = f"My name is {name}"
+
+# print(type(name))
+print(msg)
